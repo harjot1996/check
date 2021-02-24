28678556376acda2b41ed6eef339f5e717a75a45
harjot1996 <harjot1996@gmail.com>
2021-02-24 18:27:55 +0530

    app yaml updated

--- classify-meglet/main.py
+++ classify-meglet/main.py
@@ -24,26 +24,18 @@ def prediction_endpoint():
         return 'kindly send a POST request'    #return this if request is 'GET'
     elif request.method == 'POST':
         b = 0
-        print("-")
         image_file = request.files.get("input_file")
-        print("--")
         # filename = image_file.filename
         # filepath = os.path.join('./', filename)
         # image_file.save(filepath)
         # test_image = image.load_img(os.path.join('./', filename), color_mode ='rgb', target_size = (224, 224))
         img = Image.open(io.BytesIO(image_file.read()))
-        print("---")
         img = img.convert('RGB')
         img = img.resize((224, 224), Image.NEAREST)
-        print("----")
         test_image = image.img_to_array(img)
-        print("-----")
         test_image = np.expand_dims(test_image, axis = 0)
-        print("------")
         result = model.predict(test_image)
-        print("-------")
         res = np.argmax(result)
-        print("woohoo")
         #prediction 
         response = json.dumps(dict1[res])
     return response #send the model response back to the user
